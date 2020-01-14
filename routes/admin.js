7643a82a09e767d0321c9f7eaa9f4649c76ecf49
marinagotovkina <marina@uppercase.no>
2020-01-14 10:57:04 +0100

    - connect routes to html

--- routes/admin.js
+++ routes/admin.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 // This route will handle the creation of the products which admin can do
 const express = require('express');
 
@@ -6,9 +8,7 @@ const router = express.Router();
 
 // This route is reach under /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.send(
-      '<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-      );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // get makes sure that it's a get method & that it's exactly this path ('/')
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express.js</h1>');
+  // Here dirname is pointing to routes but when we add '../' it gets to views
+  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = router;
