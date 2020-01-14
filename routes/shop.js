0d99f82dce999ac265ac0ea26a928388ebaa0d05
marinagotovkina <marina@uppercase.no>
2020-01-14 11:14:08 +0100

    - add 404 page

--- routes/shop.js
+++ routes/shop.js
@@ -6,8 +6,8 @@ const router = express.Router();
 
 // get makes sure that it's a get method & that it's exactly this path ('/')
 router.get('/', (req, res, next) => {
-  // Here dirname is pointing to routes but when we add '../' it gets to views
-  res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
+  // Here dirname is pointing to routes but when we add '..' it gets to views
+  res.sendFile(path.join(__dirname, '..', 'views', 'shop.html'));
 });
 
 module.exports = router;
