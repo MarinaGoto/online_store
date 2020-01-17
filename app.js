d0d04147886ad4449bb3face4e2d40abecb97ea3
marinagotovkina <marina@uppercase.no>
2020-01-17 14:29:32 +0100

    - add Layout with Partials

--- app.js
+++ app.js
@@ -2,20 +2,13 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
 // Here we tell express that we want to compiler dynamic templates with the handlebars engine and where to find these templates ('views')
-// Here we register the engine
-app.engine(
-    'hbs',
-    expressHbs({
-      layoutsDir: 'views/layouts/',
-      defaultLayout: 'main-layout',
-      extname: 'hbs'
-    }));
-app.set('view engine', 'hbs');
+// Ejs is supported so we don't need to register engine
+
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
@@ -30,7 +23,10 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found'})
+  res.status(404).render('404', {
+    pageTitle: 'Page Not Found',
+    path: ''
+  })
 });
 
 app.listen(3000);
